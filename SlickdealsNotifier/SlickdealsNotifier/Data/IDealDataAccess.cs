5e2605f8892279e4a430cad8deb0d4f7a615c7f7
Yves Mancera <yves.mancera@gmail.com>
2020-10-04 13:42:44 -0700

    Add DAL

---
+++ SlickdealsNotifier/SlickdealsNotifier/Data/IDealDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using SlickdealsNotifier.Models;
+using System.Threading.Tasks;
+
+namespace SlickdealsNotifier.Data
+{
+    interface IDealDataAccess
+    {
+        Task<bool> IsDealNew(Deal deal);
+        Task SaveDeal(Deal deal);
+    }
+}
