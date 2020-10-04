c4ab9ff95e5f96b20de2626b173e93ecb8b96d11
Yves Mancera <yves.mancera@gmail.com>
2020-10-04 13:40:24 -0700

    Add EFCore migrations

---
+++ SlickdealsNotifier/SlickdealsNotifier/Migrations/20201003234605_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SlickdealsNotifier.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Store = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true),
+                    Votes = table.Column<int>(nullable: false),
+                    IsFire = table.Column<bool>(nullable: false),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deals");
+        }
+    }
+}
