371cb3ace3761732bfc7964465b541b5aa0d5dc0
Yves Mancera <yves.mancera@gmail.com>
2021-10-10 05:50:09 -0700

    improve logging in business and parser classes, completed pending TODO

--- src/SlickDealsNotifier/Scraping/HtmlContentParser.cs
+++ src/SlickDealsNotifier/Scraping/HtmlContentParser.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using HtmlAgilityPack;
 using HtmlAgilityPack.CssSelectors.NetCore;
+using Microsoft.Extensions.Logging;
 using SlickdealsNotifier.Models;
 
 namespace SlickdealsNotifier.Scraping
 {
     public class HtmlContentParser : IHtmlContentParser
     {
+        private readonly ILogger<HtmlContentParser> _logger;
+
+        public HtmlContentParser(
+            ILogger<HtmlContentParser> logger)
+        {
+            _logger = logger;
+        }
+
         public IReadOnlyCollection<Deal> Parse(HtmlDocument document)
         {
             var nodes = document.DocumentNode
@@ -25,49 +34,57 @@ namespace SlickdealsNotifier.Scraping
                 .Where(deal => deal != null)
                 .ToList();
 
+            _logger.LogInformation($"Successfully parsed {deals.Count} deals out of {dealsList.Count()} nodes");
+
             return deals.AsReadOnly();
         }
 
         private Deal ParseNode(HtmlNode node)
         {
+            // declare outside of try scope so catch block can log
+            // any info we had up until the exceptoin was thrown, for easier troubleshooting of edge cases
+            var deal = new Deal();
+
             try
             {
                 // <a href="{url}">{Title}</a>
                 var titleHtmlNode = node.QuerySelector(".bp-c-card_title");
 
-                var title = titleHtmlNode.InnerText;
-                var url = titleHtmlNode.GetAttributeValue("href", string.Empty);
+                // sometimes Slickdeals puts ads or featured promos that look like deals but don't have titles
+                // ignore those and avoid catch
+                if (titleHtmlNode == null)
+                {
+                    _logger.LogDebug($"Found a node with empty title. Skip parsing. Node InnerText: {node.InnerText}");
+                    return null;
+                }
+
+                deal.Title =titleHtmlNode.InnerText;
+                deal.Url = titleHtmlNode.GetAttributeValue("href", string.Empty);
 
                 // <button>{Store}</button>
-                var store = node.QuerySelector(".bp-p-storeLink").InnerText;
+                deal.Store = node.QuerySelector(".bp-p-storeLink").InnerText;
                 
                 // <span>{price}</span>
-                var price = node.QuerySelector(".bp-p-dealCard_price").InnerText;
+                deal.Price = node.QuerySelector(".bp-p-dealCard_price").InnerText;
 
                 // <span>{Votes}</span>
                 var votesString = node.QuerySelector(".bp-p-votingThumbsPopup_voteCount").InnerText;
                 int.TryParse(votesString, out var votes);
+                deal.Votes = votes;
 
                 // <span class="..."></span> -- this one is only present when the deal is on fire
-                var isFire = node.QuerySelectorAll(".bp-c-icon bp-i-fire")
+                deal.IsFire = node.QuerySelectorAll(".bp-c-icon bp-i-fire")
                                  .Count > 0;
 
-                return new Deal
-                {
-                    Title = title,
-                    Url = url,
-                    IsFire = isFire,
-                    Price = price,
-                    Store = store,
-                    Votes = votes
-                };
+                return deal;
+
             }
+
             // sometimes slickdeals has some special deals where their html looks different, handle
             // gracefully by allowing to process other deals if one fails
             catch (Exception e)
             {
-                //TODO serilog
-                Console.WriteLine($"Error parsing node. {e.Message} {e.StackTrace}");
+                _logger.LogError($"Error parsing deal node. Deal up to exception thrown is {deal}. Exception: {e.Message}{Environment.NewLine} {e.StackTrace}");
                 return null;
             }
         }
